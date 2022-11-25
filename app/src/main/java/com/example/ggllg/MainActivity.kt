f79ae283098d0f5592ad695b4d376353c7e6af7f
ItzRig3l <itzrigel113@gmail.com>
2022-11-25 09:47:07 +0700

    Initial commit

---
+++ app/src/main/java/com/example/ggllg/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.ggllg
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
